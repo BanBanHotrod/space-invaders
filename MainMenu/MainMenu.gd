496f999ce3f90644b1c0caea72de8855a0776a73
cjclifford <cliffordcuan@gmail.com>
2021-08-28 21:33:10 +0200

    Adds a main menu

---
+++ MainMenu/MainMenu.gd
@@ -0,0 +1,22 @@
+extends MarginContainer
+
+
+var audio_player: AudioStreamPlayer
+
+
+func _ready():
+  $AudioStreamPlayer.play()
+
+
+func _on_Button_pressed():
+  $AudioStreamPlayer.stop()
+  var return_value = get_tree().change_scene("res://Game/Game3D.tscn")
+
+  if return_value != OK:
+    print("Error changing scene:", return_value)
+    get_tree().quit()
+
+
+func _on_Button2_pressed():
+  $AudioStreamPlayer.stop()
+  get_tree().quit()
