1ba1d20d3507be485a104fd489a73f59bd5c2c1a
cjclifford <cliffordcuan@gmail.com>
2021-08-29 14:12:43 +0200

    Improves project structure

---
+++ entities/weapons/Weapon.gd
@@ -0,0 +1,36 @@
+extends Spatial
+class_name Weapon
+
+
+export var projectile = preload("res://entities/projectiles/simple_projectile/SimpleProjectile.tscn")
+export (bool) var automatic = true
+export (float) var fire_rate = 0.1
+export (AudioStreamSample) var sound_fire
+
+
+var fire_rate_timer = 0.0
+var attacking: bool = false
+
+
+func _ready() -> void:
+  pass
+
+
+func _physics_process(delta: float) -> void:
+  fire_rate_timer += delta
+
+  if attacking and fire_rate_timer >= fire_rate:
+    $AudioStreamPlayer.stream = self.sound_fire
+    $AudioStreamPlayer.play()
+    var new_projectile = projectile.instance()
+    Global.root.add_child(new_projectile)
+    new_projectile.translation = global_transform.origin
+    fire_rate_timer = 0.0
+
+
+func attack() -> void:
+  attacking = true
+
+
+func attack_stop() -> void:
+  attacking = false
