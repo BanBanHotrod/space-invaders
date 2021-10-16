7a9f3ab2352c273c3356762f2e0ec8057398be52
cjclifford <cliffordcuan@gmail.com>
2021-10-16 22:40:43 +0200

    Adds enemy attacks

--- entities/weapons/Weapon.gd
+++ entities/weapons/Weapon.gd
@@ -2,7 +2,7 @@ extends Spatial
 class_name Weapon
 
 
-export var projectile = preload("res://entities/projectiles/simple_projectile/SimpleProjectile.tscn")
+export (PackedScene) var projectile
 export (bool) var automatic = true
 export (float) var fire_rate = 0.1
 export (AudioStreamSample) var sound_fire
@@ -12,23 +12,26 @@ var fire_rate_timer = 0.0
 var attacking: bool = false
 
 
-func _ready() -> void:
-  pass
-
-
 func _physics_process(delta: float) -> void:
   fire_rate_timer += delta
 
   if attacking and fire_rate_timer >= fire_rate:
-    $AudioStreamPlayer.stream = self.sound_fire
-    $AudioStreamPlayer.play()
-    var new_projectile = projectile.instance()
-    Global.root.add_child(new_projectile)
-    new_projectile.translation = global_transform.origin
-    fire_rate_timer = 0.0
+    self.attack()
 
 
 func attack() -> void:
+  $AudioStreamPlayer.stream = self.sound_fire
+  $AudioStreamPlayer.play()
+
+  var new_projectile = projectile.instance()
+
+  Global.root.add_child(new_projectile)
+
+  new_projectile.translation = global_transform.origin
+  fire_rate_timer = 0.0
+
+
+func attack_start() -> void:
   attacking = true
 
 
