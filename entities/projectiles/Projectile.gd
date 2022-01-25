566bb8cf5e2b316509afdf01f0acf59234e6397f
cjclifford <cliffordcuan@gmail.com>
2022-01-26 00:42:35 +0200

    Gives projectiles damage

--- entities/projectiles/Projectile.gd
+++ entities/projectiles/Projectile.gd
@@ -4,38 +4,25 @@ class_name Projectile
 
 export (float) var speed = 1.0
 export (float) var max_time = 3.0
-
+export (float) var damage = 1.0
 
 var velocity := Vector2(0.0, -1.0)
 var time := 0.0
-
-
-func _ready() -> void:
-  self.velocity = -global_transform.y
-
-
-func move(delta: float) -> void:
-  self.position += self.velocity.normalized() * speed * delta
 
 
 func _process(delta: float) -> void:
   time += delta
 
   if time >= max_time:
-    self.queue_free()
+    queue_free()
 
 
 func _physics_process(delta: float) -> void:
-  move(delta)
+  var collision = move_and_collide(velocity.normalized() * speed * delta)
 
+  if collision:
+    var collider = collision.collider
 
-# func _on_projectile_area_entered(area):
-#   match area.collision_layer:
-#     0b10, 0b100000:
-#       queue_free()
-#     _:
-#       pass
-
-
-func _on_projectile_area_entered(_area):
-  queue_free()
+    if collider.is_in_group('enemy'):
+      queue_free()
+      collider.take_damage(damage)
