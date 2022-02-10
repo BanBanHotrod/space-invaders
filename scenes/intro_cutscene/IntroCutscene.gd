8d3bc7cf6a073b33a822095fbec5c2f28c32bea7
cjclifford <cliffordcuan@gmail.com>
2022-02-11 01:22:32 +0200

    Adds intro video

---
+++ scenes/intro_cutscene/IntroCutscene.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func _on_VideoPlayer_finished():
+  var return_value = get_tree().change_scene('res://scenes/game/Game.tscn')
+  if return_value != OK:
+    print('Error changing scene:', return_value)
+    get_tree().quit()
