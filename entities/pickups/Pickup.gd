ff43079b9f7674fc99d5e0bbe98dca2bee3c5003
cjclifford <cliffordcuan@gmail.com>
2022-02-19 12:06:21 +0200

    Adds player pickups

--- entities/pickups/Pickup.gd
+++ entities/pickups/Pickup.gd
@@ -1,15 +1,48 @@
-extends KinematicBody2D
+extends Area2D
 
 
-const velocity := Vector2.DOWN
-const speed := 200.0
+const direction := Vector2.DOWN
+const speed := 100.0
 
 
-func _physics_process(delta):
-  var collision = move_and_collide(velocity.normalized() * speed * delta)
-  
-  if collision:
-    var collider = collision.collider
-    
-    if collider.is_in_group('player'):
-      queue_free()
+func _process(delta):
+  var velocity = direction.normalized() * speed
+
+  position += velocity * delta
+
+
+func _on_PickupBrandon_body_entered(body):
+  if body != Global.root.player_brandon:
+    Global.root.set_current_player(Global.root.player_brandon)
+
+  Global.root.current_player.upgrade_weapon()
+  queue_free()
+
+
+func _on_PickupCarro_body_entered(body):
+  if body != Global.root.player_carro:
+    Global.root.set_current_player(Global.root.player_carro)
+
+  Global.root.current_player.upgrade_weapon()
+  queue_free()
+
+
+func _on_PickupConrad_body_entered(body):
+  if body != Global.root.player_conrad:
+    Global.root.set_current_player(Global.root.player_conrad)
+
+  Global.root.current_player.upgrade_weapon()
+  queue_free()
+
+
+func _on_PickupKyle_body_entered(body):
+  if body != Global.root.player_kyle:
+    Global.root.set_current_player(Global.root.player_kyle)
+
+  Global.root.current_player.upgrade_weapon()
+  queue_free()
+
+
+func _on_PickupUniversal_body_entered(body):
+  body.upgrade_weapon()
+  queue_free()
