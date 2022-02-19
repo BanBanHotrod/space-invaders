ec79b11d76c52680964298712e42b7537751fd5e
cjclifford <cliffordcuan@gmail.com>
2022-02-19 12:10:00 +0200

    Improves player projectiles

--- entities/projectiles/Projectile.gd
+++ entities/projectiles/Projectile.gd
@@ -2,12 +2,21 @@ extends Area2D
 class_name Projectile
 
 
-export (Vector2) var direction = Vector2.UP
 export (float) var speed = 100.0
 export (float) var max_time = 3.0
-export (float) var damage = 1.0
+export (float) var damage = 100.0
+export (bool) var position_globally = true
+export (float) var accuracy_variance = 0.0
 
 var time := 0.0
+
+
+func _ready():
+  assert($CollisionShape2D != null)
+  
+  if accuracy_variance:
+    var deviation = (randi() % int(accuracy_variance * 100 * 2) - int(accuracy_variance * 100)) / 666.0
+    rotate(deviation)
 
 
 func _process(delta):
@@ -15,9 +24,9 @@ func _process(delta):
 
   if time >= max_time:
     queue_free()
-
-
-func _physics_process(delta):
+  
+  var up_rotation = rotation - deg2rad(90)
+  var direction := Vector2(cos(up_rotation), sin(up_rotation))
   var velocity = direction.normalized() * speed
   position += velocity * delta
 
@@ -25,6 +34,6 @@ func _physics_process(delta):
 func die():
   speed = 0.0
   hide()
-  $CollisionShape2D.disabled = true
+  $CollisionShape2D.set_deferred("disabled", true)
   set_process(false)
   queue_free()
--- entities/projectiles/player_projectiles/PlayerProjectile.gd
+++ entities/projectiles/player_projectiles/PlayerProjectile.gd
@@ -1,5 +1,8 @@
 extends Projectile
 
 
-func _on_ProjectileBrandon_body_entered(body):
-  print('body entered')
+func _on_Projectile_body_entered(body):
+  print('body', body)
+  if body.is_in_group('enemy'):
+    die()
+    body.take_damage(damage)
