6e4a82bc1c8f996d78d26aec4efd4be5f1c5df9e
C. Clifford <cliffordcuan@gmail.com>
2022-02-19 19:10:58 +0200

    Fixes indentation

--- menus/main_menu/MainMenu.gd
+++ menus/main_menu/MainMenu.gd
@@ -5,19 +5,19 @@ var audio_player: AudioStreamPlayer
 
 
 func _ready():
-  $AudioStreamPlayer.play()
+	$AudioStreamPlayer.play()
 
 
 func _on_Start_pressed():
-  $AudioStreamPlayer.stop()
+	$AudioStreamPlayer.stop()
 
-  #var return_value = get_tree().change_scene('res://scenes/intro_cutscene/IntroCutscene.tscn')
-  var return_value = get_tree().change_scene('res://scenes/game/Game.tscn')
-  if return_value != OK:
-    print('Error changing scene:', return_value)
-    get_tree().quit()
+	#var return_value = get_tree().change_scene('res://scenes/intro_cutscene/IntroCutscene.tscn')
+	var return_value = get_tree().change_scene('res://scenes/game/Game.tscn')
+	if return_value != OK:
+		print('Error changing scene:', return_value)
+		get_tree().quit()
 
 
 func _on_Quit_pressed():
-  $AudioStreamPlayer.stop()
-  get_tree().quit()
+	$AudioStreamPlayer.stop()
+	get_tree().quit()
