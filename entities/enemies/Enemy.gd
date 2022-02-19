dd3c8a5bcd15e69557cf84b1559284d643e71152
cjclifford <cliffordcuan@gmail.com>
2022-02-19 12:06:55 +0200

    Improves enemy script

--- entities/enemies/Enemy.gd
+++ entities/enemies/Enemy.gd
@@ -1,32 +1,43 @@
-extends KinematicBody2D
+extends Area2D
 class_name Enemy
 
 
-export (float) var speed = 1.0
-export (AudioStreamSample) var sound_death
-export (Array, PackedScene) var power_ups = []
+export (int) var total_health = 100
+export (int) var score = 100
+export (bool) var auto_attack = true
 
-onready var velocity := Vector2.ZERO
-onready var attack_cooldown := 0
-onready var health = 1.0
+var pickup = null
+var velocity := Vector2.ZERO
+var attack_cooldown := 0
 
+onready var weapon_cooldown_timer = $WeaponCooldownTimer
+onready var damage_cooldown_timer = $DamageCooldownTimer
+onready var collision_shape_2d = $CollisionShape2D
+onready var weapon = $Weapon
+onready var color_material = $AnimatedSprite.material
+onready var animated_sprite = $AnimatedSprite
+onready var health = total_health 
 
-signal enemy_destroyed(enemy)
+signal enemy_destroyed(enemy, killed_by_player)
 
 
 func _ready():
-  assert($Timer != null)
-  assert($CollisionShape2D != null)
-  assert($AudioStreamPlayer != null)
+  assert(weapon_cooldown_timer != null)
+  assert(damage_cooldown_timer != null)
+  assert(collision_shape_2d != null)
+  #assert(weapon != null)
+  assert(color_material != null)
+  assert(animated_sprite != null)
+  
+  if auto_attack:
+    attack_cooldown = randi() % 100 + 1
 
-  attack_cooldown = randi() % 100
-
-  $Timer.wait_time = attack_cooldown
-  $Timer.start()
+    weapon_cooldown_timer.wait_time = attack_cooldown
+    weapon_cooldown_timer.start()
 
 
 func attack():
-  $Weapon.attack()
+  weapon.attack()
 
 
 func take_damage(damage):
@@ -34,52 +45,77 @@ func take_damage(damage):
   
   if health <= 0:
     die()
+    
+  modulate.g = 0.5
+  modulate.b = 0.5
+  
+  #color_material.set_shader_param("brightness", 0.5)
+  $DamageCooldownTimer.start()
+  collision_shape_2d.set_deferred("disabled", true)
 
 
-func die():
-  if $CollisionShape2D.disabled:
-    return
-
-  $AudioStreamPlayer.play()
-
+func die(killed_by_player=true):
+  disable_collision()
   hide()
-  $CollisionShape2D.disabled = true
 
-  yield($AudioStreamPlayer, 'finished')
-  emit_signal('enemy_destroyed', self)
   queue_free()
 
-  Global.add_score(10)
+  emit_signal('enemy_destroyed', self, killed_by_player)
   
-  if true or randi() % 200 == 0:
-    var random_powerup = randi() % power_ups.size()
-    var power_up_instance = power_ups[random_powerup].instance()
+  if pickup != null:
+    var pickup_instance = pickup.instance()
     
-    power_up_instance.position = position
-    Global.root.add_child(power_up_instance)
-  elif power_ups.size() == 1:
-    var power_up_instance = power_ups[0].instance()
-    
-    power_up_instance.position = position
-    Global.root.add_child(power_up_instance)
+    pickup_instance.position = position
+    Global.root.add_child(pickup_instance)
 
 
 func move(move_velocity: Vector2):
-  return
-  var collision = move_and_collide(move_velocity)
-
-  if collision:
-    var collider = collision.collider
-
-    if collider.is_in_group('projectile'):
-      take_damage(collider.damage)
-      collider.die()
-
-    if collider.is_in_group('player'):
-      die()
-      collider.die()
+  position += move_velocity
+  if position.y < 0:
+    disable_collision()
+  elif collision_disabled():
+    enable_collision()
+    
+  if position.y > 720 + 100:
+    die(false)
 
 
-func _on_Timer_timeout():
+func collision_disabled():
+  return collision_shape_2d.disabled
+
+  
+func enable_collision():
+  collision_shape_2d.set_deferred("disabled", false)
+  
+  
+func disable_collision():
+  collision_shape_2d.set_deferred("disabled", true)
+
+
+func _on_DamageCooldownTimer_timeout():
+  modulate.g = 1.0
+  modulate.b = 1.0
+  #color_material.set_shader_param("brightness", 0.0)
+  collision_shape_2d.set_deferred("disabled", false)
+
+
+func _on_WeaponCooldownTimer_timeout():
   if position.y > 0:
     attack()
+    
+  weapon_cooldown_timer.start()
+
+
+func _on_Enemy_area_entered(area):
+  if area.is_in_group('projectile'):
+      take_damage(area.damage)
+      area.die()
+
+
+func _on_Enemy_body_entered(body):
+  if body.is_in_group('player'):
+    if body.grace:
+      return
+  
+    die()
+    body.die()
