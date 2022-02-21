eb361388b4c06e0614bd3419ab131f5558baca94
C. Clifford <cliffordcuan@gmail.com>
2022-02-21 11:04:19 +0200

    Format's Game script

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -1,7 +1,6 @@
 extends Node2D
 class_name Game
 
-
 var current_player = null
 var players := []
 
@@ -18,123 +17,121 @@ onready var music_player = $MusicPlayer
 onready var boss_health_bar_parent = $CanvasLayer/UI/BossHealthBar
 onready var boss_health_bar = $CanvasLayer/UI/BossHealthBar/HealthBarBoss
 
-
 signal root_initialized
 signal input_attack_start
 signal input_attack_stop
 
 
 func _ready():
-  assert(player_brandon != null)
-  assert(player_carro != null)
-  assert(player_conrad != null)
-  assert(player_kyle != null)
-  assert(ui_brandon_skull != null)
-  assert(ui_carro_skull != null)
-  assert(ui_conrad_skull != null)
-  assert(ui_kyle_skull != null)
-  assert(announcer != null)
-  assert(music_player != null)
-  assert(boss_health_bar != null)
-  assert(boss_health_bar_parent != null)
-  
-  boss_health_bar_parent.hide()
+	assert(player_brandon != null)
+	assert(player_carro != null)
+	assert(player_conrad != null)
+	assert(player_kyle != null)
+	assert(ui_brandon_skull != null)
+	assert(ui_carro_skull != null)
+	assert(ui_conrad_skull != null)
+	assert(ui_kyle_skull != null)
+	assert(announcer != null)
+	assert(music_player != null)
+	assert(boss_health_bar != null)
+	assert(boss_health_bar_parent != null)
 
-  Global.root = self
-  Global.emit_signal('root_initialized')
+	boss_health_bar_parent.hide()
 
-  players.append(player_brandon)
-  players.append(player_carro)
-  players.append(player_conrad)
-  players.append(player_kyle)
+	Global.root = self
+	Global.emit_signal("root_initialized")
 
-  for player in players:
-    player.despawn()
+	players.append(player_brandon)
+	players.append(player_carro)
+	players.append(player_conrad)
+	players.append(player_kyle)
 
-  current_player = player_conrad
-  current_player.spawn()
+	for player in players:
+		player.despawn()
 
-  randomize()
+	current_player = player_conrad
+	current_player.spawn()
 
-  player_brandon.connect('player_died', self, '_on_Brandon_died')
-  player_carro.connect('player_died', self, '_on_Carro_died')
-  player_conrad.connect('player_died', self, '_on_Conrad_died')
-  player_kyle.connect('player_died', self, '_on_Kyle_died')
+	randomize()
+
+	player_brandon.connect("player_died", self, "_on_Brandon_died")
+	player_carro.connect("player_died", self, "_on_Carro_died")
+	player_conrad.connect("player_died", self, "_on_Conrad_died")
+	player_kyle.connect("player_died", self, "_on_Kyle_died")
 
 
 func _process(_delta):
-  process_input()
+	process_input()
 
 
 func respawn_player():
-  if current_player.total_lives > 0:
-    current_player.spawn()
-    current_player.enable_grace()
-  else:
-    var next_player = get_next_player()
+	if current_player.total_lives > 0:
+		current_player.spawn()
+		current_player.enable_grace()
+	else:
+		var next_player = get_next_player()
 
-    if next_player != null:
-      next_player.spawn()
-      next_player.enable_grace()
-    else:
-      var return_value := get_tree().change_scene('res://menus/main_menu/MainMenu.tscn')
-  
-      if return_value != OK:
-        print('Error changing scene:', return_value)
-        get_tree().quit()
-        
-    current_player = next_player
+		if next_player != null:
+			next_player.spawn()
+			next_player.enable_grace()
+		else:
+			var return_value := get_tree().change_scene("res://menus/main_menu/MainMenu.tscn")
+
+			if return_value != OK:
+				print("Error changing scene:", return_value)
+				get_tree().quit()
+
+		current_player = next_player
 
 
 func get_next_player():
-  for player in players:
-    if player.total_lives > 0:
-      return player
+	for player in players:
+		if player.total_lives > 0:
+			return player
 
-  return null
+	return null
 
 
 func process_input():
-  if not Global.controls:
-    return
+	if not Global.controls:
+		return
 
-  if Input.is_action_pressed('action_attack'):
-    emit_signal('input_attack_start')
+	if Input.is_action_pressed("action_attack"):
+		emit_signal("input_attack_start")
 
-  if Input.is_action_just_released('action_attack'):
-    emit_signal('input_attack_stop')
+	if Input.is_action_just_released("action_attack"):
+		emit_signal("input_attack_stop")
 
-  if Input.is_action_pressed('ui_cancel'):
-    var result_value = get_tree().change_scene('res://menus/main_menu/MainMenu.tscn')
+	if Input.is_action_pressed("ui_cancel"):
+		var result_value = get_tree().change_scene("res://menus/main_menu/MainMenu.tscn")
 
-    if result_value != OK:
-      get_tree().quit()
+		if result_value != OK:
+			get_tree().quit()
 
 
 func set_current_player(player):
-  current_player.weapon.attack_stop()
-  current_player.despawn()
-  current_player = player
-  current_player.spawn()
-  print('current player', current_player)
-  
-  
+	current_player.weapon.attack_stop()
+	current_player.despawn()
+	current_player = player
+	current_player.spawn()
+	print("current player", current_player)
+
+
 func announce_wave(wave_number, title):
-  announcer.announce_wave(wave_number, title)
+	announcer.announce_wave(wave_number, title)
 
 
 func _on_Brandon_died():
-  ui_brandon_skull.show()
-      
-      
-func _on_Carro_died():
-  ui_carro_skull.show()
-      
-      
-func _on_Conrad_died():
-  ui_conrad_skull.show()
-      
-      
-func _on_Kyle_died():
-  ui_kyle_skull.show()
+	ui_brandon_skull.show()
 
+
+func _on_Carro_died():
+	ui_carro_skull.show()
+
+
+func _on_Conrad_died():
+	ui_conrad_skull.show()
+
+
+func _on_Kyle_died():
+	ui_kyle_skull.show()
