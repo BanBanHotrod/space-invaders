8703087b5060955dd971f52670194790ce91dee9
C. Clifford <cliffordcuan@gmail.com>
2022-02-21 16:45:53 +0200

    Adds switching between wepaons on DJ

--- entities/enemies/dj/DJ.gd
+++ entities/enemies/dj/DJ.gd
@@ -2,27 +2,30 @@ extends Enemy
 
 enum BossStates {
 	DEFAULT,
-	SUMMON_UFOS,
 	LASER_ATTACK,
 	VINYL_ATTACK,
 }
 
-export(float) var entrance_speed = 35
+export(float) var entrance_speed = 350
 
-onready var ufo_weapon = $WeaponUFO
 onready var laser_weapon = $WeaponLaser
 onready var vinyl_weapon = $WeaponVinyl
+onready var laser_weapon_timer = $LaserWeaponTimer
+onready var vinyl_weapon_timer = $VinylWeaponTimer
 
 var current_boss_state = BossStates.DEFAULT
 var in_position := false
 
 
 func _ready():
-	assert(ufo_weapon != null)
 	assert(laser_weapon != null)
 	assert(vinyl_weapon != null)
+	assert(laser_weapon_timer != null)
+	assert(vinyl_weapon_timer != null)
 
 	invincible = true
+
+	Global.root.boss_health_bar_parent.show()
 
 
 func _process(delta):
@@ -31,34 +34,66 @@ func _process(delta):
 	elif not in_position and position.y >= 100:
 		invincible = false
 		in_position = true
-		weapon_cooldown_timer.wait_time = 0.01
-		weapon_cooldown_timer.start()
 		animated_sprite.play()
-	Global.root.boss_health_bar_parent.show()
+		laser_weapon.enable_weapon()
+		laser_weapon_timer.start()
 
 
 func die(killed_by_player = false):
 	Global.root.boss_health_bar_parent.hide()
-	pass
+	disable_collision()
+	hide()
+	queue_free()
+	emit_signal("enemy_destroyed", self, killed_by_player)
+
+	if pickup != null:
+		var pickup_instance = pickup.instance()
+
+		pickup_instance.position = position
+		Global.root.add_child(pickup_instance)
 
 
 func take_damage(damage):
-	.take_damage(damage)
+	if invincible:
+		return
+
+	health -= damage
+
+	if health <= 0:
+		die(true)
+
+	modulate.g = 0.5
+	modulate.b = 0.5
+
+	$DamageCooldownTimer.start()
+	collision_shape_2d.set_deferred("disabled", true)
 
 	Global.root.boss_health_bar.rect_scale.x = health / total_health
 
 
 func attack():
-	# TODO: laser weapon
-	if not laser_weapon.weapon_enabled:
-		laser_weapon.enable_weapon()
-
-
-#vinyl_weapon.attack()
+	if vinyl_weapon.weapon_enabled:
+		vinyl_weapon.attack()
 
 
 func _on_WeaponCooldownTimer_timeout():
-	if position.y > 0:
+	if in_position:
 		attack()
 
 	weapon_cooldown_timer.start()
+
+
+func _on_LaserWeaponTimer_timeout():
+	if in_position:
+		vinyl_weapon_timer.start()
+		weapon_cooldown_timer.start()
+		laser_weapon.disable_weapon()
+		vinyl_weapon.enable_weapon()
+
+
+func _on_VinylWeaponTimer_timeout():
+	if in_position:
+		laser_weapon_timer.start()
+		weapon_cooldown_timer.stop()
+		vinyl_weapon.disable_weapon()
+		laser_weapon.enable_weapon()
--- entities/weapons/enemy_weapons/dj/WeaponDJVinyl.gd
+++ entities/weapons/enemy_weapons/dj/WeaponDJVinyl.gd
@@ -1,5 +1,17 @@
 extends Weapon
 
+var weapon_enabled := false
+
+
+func enable_weapon():
+	set_process(true)
+	weapon_enabled = true
+
+
+func disable_weapon():
+	set_process(false)
+	weapon_enabled = false
+
 
 func _process(delta):
 	rotate(1.618033988749894 * 1.2 * delta)
