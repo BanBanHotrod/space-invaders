2f1c7013efb79fa859a4e52649063e3237bac876
cjclifford <cliffordcuan@gmail.com>
2022-02-22 03:07:08 +0200

    Adds cookie points

---
+++ entities/points/Cookie.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+
+var score := 100
+var velocity := Vector2.ZERO
+
+func _ready():
+	velocity = Vector2((randi() % 80) - 40, randi() % 200 + 100)
+	
+	apply_impulse(Vector2.ZERO, velocity)
+
+
+func die():
+	queue_free()
+
+
+func _on_Timer_timeout():
+	queue_free()
+
+
+func _on_Points_body_entered(body):
+	print(body)
+	if body.is_in_group("player"):
+		Global.add_score(score)
+		die()
