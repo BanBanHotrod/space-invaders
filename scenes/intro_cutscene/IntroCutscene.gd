03f6688817944f08e2b3f4fe48395eea084cebd9
C. Clifford <cliffordcuan@gmail.com>
2022-02-26 16:40:42 +0200

    Makes cutscene skippable

--- scenes/intro_cutscene/IntroCutscene.gd
+++ scenes/intro_cutscene/IntroCutscene.gd
@@ -1,7 +1,23 @@
 extends Control
 
+onready var video_player = $VideoPlayer
+
+
+func _ready():
+	assert(video_player != null)
+
+
+func _input(_event):
+	if Input.is_action_pressed("ui_select"):
+		video_player.stop()
+		_exit_scene()
+
 
 func _on_VideoPlayer_finished():
+	_exit_scene()
+
+
+func _exit_scene():
 	var return_value = get_tree().change_scene("res://scenes/game/Game.tscn")
 	if return_value != OK:
 		print("Error changing scene:", return_value)
